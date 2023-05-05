f04c2ddc2b0ef286fe0c60593ecb580523202557
ahalajian <andrewhalajian19@gmail.com>
2023-05-05 17:30:12 -0400

    Revise db.py with updated columns and with color

--- src/db.py
+++ src/db.py
@@ -13,9 +13,8 @@ Overview of Data Structuring:
 association_table_receiver = db.Table(
     "receiver_association",
     db.Column("event_id", db.Integer, db.ForeignKey("event.id")),
-    db.Column("receivers", db.Integer, db.ForeignKey("user.id"))
+    db.Column("receiver_emails", db.Integer, db.ForeignKey("user.email"))
 )
-
 
 # classes for tables here
 class Event(db.Model):
@@ -25,37 +24,36 @@ class Event(db.Model):
   __tablename__ = "event"
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   name = db.Column(db.String, nullable=False)  # title of event
+  color = db.Column(db.String, nullable = False)
   start_time = db.Column(db.DateTime, nullable=False)
   end_time = db.Column(db.DateTime, nullable=False)
-  sender_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-  receivers = db.relationship(
-      "User", secondary=association_table_receiver, back_populates="events_pending")
-  message = db.Column(db.String, nullable=True)
-  accepted = db.Column(db.Boolean, nullable=False)
+  sender_email = db.Column(db.Integer, db.ForeignKey("user.email"), nullable=False)
+  receiver_emails = db.relationship(
+      "User", secondary=association_table_receiver, back_populates="events_received")
 
   def __init__(self, **kwargs):
     """
     Initializes an event object
     """
-    self.name = kwargs.get("name", "Event Request")
+    self.name = kwargs.get("name", "Event")
+    self.color = kwargs.get("color", "Blue")
     # What would be the best way for the client to put in times? if start and end time are left empty returns an error
     self.start_time = kwargs.get("start_time")
     self.end_time = kwargs.get("end_time")
     # should also return error if no receiver_id specified
-    self.sender_id = kwargs.get("sender_id")
+    self.sender_email = kwargs.get("sender_email")
     # should also return error if no receiver_id specified
-    self.receivers = kwargs.get("receivers", "")
-    self.message = kwargs.get("message")
-    self.accepted = kwargs.get("accepted")
+    self.receiver_emails = kwargs.get("receiver_emailss", "")
 
   def serialize(self):
     """
     Serializes an Event object
     """
-    return {"id": self.id, "name": self.name, "start_time": self.start_time, 
-            "end_time": self.end_time, "sender_id": self.sender_id, 
-            "receivers": [r.simple_serialize() for r in self.receivers], "message": self.message, 
-            "accepted": self.accepted }
+    return {"id": self.id, "name": self.name, 
+            "start_time": self.start_time, 
+            "end_time": self.end_time, 
+            "sender_email": self.sender_email, 
+            "receiver_emails": [r.simple_serialize() for r in self.receiver_emails]}
 
 
 class User(db.Model):
@@ -63,34 +61,41 @@ class User(db.Model):
   User Model
   """
   __tablename__ = "user"
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  # the usernames won't be unique, but we will just go based off of ids
-  username = db.Column(db.String, nullable=False)
-  password = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, nullable=False, primary_key = True)
   # if user gets deleted, then events accepted and pending should get deleted too
-  events_accepted = db.relationship("Event", cascade="delete")
-  events_pending = db.relationship(
+  events_sent = db.relationship("Event", cascade="delete")
+  events_received = db.relationship(
       "Event", secondary=association_table_receiver, back_populates="receivers", cascade="delete")
 
   def __init__(self, **kwargs):
     """
     Initializes a user object
     """
-    self.username = kwargs.get("username", "")
-    self.password = kwargs.get("password", "")
+    self.email = kwargs.get("email")
+
+  def get_both_events(self):
+    """
+    Returns all the events associated with this User
+    """
+    both_events = []
+    for c in self.events_sent:
+      both_events.append(c)
+    for i in self.events_received:
+      both_events.append(i)
+    return (both_events)
 
   def serialize(self):
     """
     Serializes a User object
     """
-    return {"id": self.id, "username": self.username, "password": self.password, "events_accepted":[e.serialize() for e in self.events_accepted], "events_pending":[e.serialize() for e in self.events_pending]}
-  #Should we be serializing the password?
+    return {"email": self.email, "events":[e.serialize() for e in self.get_both_events]}
+
 
   def simple_serialize(self):
     """
     Simple serializes a user object
     """
-    return {"id": self.id, "username": self.username}
+    return {"email": self.email}
  
 
 
